79440c19221e33f0b38306507b98f430911cfc51
misaelriojasftf <48217522+misaelriojasftf@users.noreply.github.com>
2021-10-07 23:16:51 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:app_comunic/app/inject_dependences.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'app/data/data/search_repository.dart';
-import 'app/domain/repositories/search_bloc.dart';
-import 'app/domain/repositories/search_states.dart';
 import 'app/my_app.dart';
-import 'app/ui/pages/list/insert_screen.dart';
+import 'app/services/trial/trial_mode.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
